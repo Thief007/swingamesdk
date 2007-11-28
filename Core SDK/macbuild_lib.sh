eee9a3abac2cf0e86c10f2be63a62224b36bda45
aki.sexy <aki.sexy@c530d8a1-c93f-0410-abe6-152cf4f4075f>
2007-11-28 04:02:57 +0000

    New macbuild_lib.sh with -clean option

--- Core SDK/macbuild_lib.sh
+++ Core SDK/macbuild_lib.sh
@@ -1,19 +1,28 @@
 #!/bin/sh
-
 Output=`pwd`/bin/mac/
-
 cd lib/Pas/
-
-if [ #0 == "-clean" ]
+if [ $1 = "-clean" ]
 then
-	
+	cd "$Output"
+	rm -f SGSDK_Core.*
+	rm -f SGSDK_Graphics.*
+	rm -f SGSDK_Font.*
+	rm -f SGSDK_Input.*
+	rm -f SGSDK_Physics.*
+	rm -f SGSDK_Audio.*
+	rm -f sdl_image.*
+	rm -f sdl_mixer.*
+	rm -f sdl_ttf.*
+	rm -f sdl.*
+	rm -f SDLEventProcessing.*
+	echo Cleaned
+else
+	mkdir -p "$Output"
+	fpc -Mdelphi -FE"$Output" SGSDK_Core.pas
+	fpc -Mdelphi -FE"$Output" SGSDK_Graphics.pas
+	fpc -Mdelphi -FE"$Output" SGSDK_Font.pas
+	fpc -Mdelphi -FE"$Output" SGSDK_Input.pas
+	fpc -Mdelphi -FE"$Output" SGSDK_Physics.pas
+	fpc -Mdelphi -FE"$Output" SGSDK_Audio.pas
 fi
-
-mkdir -p "$Output"
-
-fpc -Mdelphi -FE"$Output" SGSDK_Core.pas
-fpc -Mdelphi -FE"$Output" SGSDK_Graphics.pas
-fpc -Mdelphi -FE"$Output" SGSDK_Font.pas
-fpc -Mdelphi -FE"$Output" SGSDK_Input.pas
-fpc -Mdelphi -FE"$Output" SGSDK_Physics.pas
-fpc -Mdelphi -FE"$Output" SGSDK_Audio.pas
+#open "$Output"
