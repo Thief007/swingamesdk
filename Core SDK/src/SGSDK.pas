860a26f5b34dd979fb36f040523f4ad589bf97ea
grinneh <grinneh@c530d8a1-c93f-0410-abe6-152cf4f4075f>
2007-11-29 01:41:30 +0000

    Core Interface complete

--- Core SDK/src/SGSDK.pas
+++ Core SDK/src/SGSDK.pas
@@ -1,6 +1,7 @@
 library SGSDK;
 
-uses SGSDK_Core, SGSDK_Input;
+uses SGSDK_Core, SGSDK_Input,
+	SDL_Mixer, SDL, SDL_Image, SDL_TTF, SDLEventProcessing;
 
 	//CORE
 
@@ -33,13 +34,128 @@ uses SGSDK_Core, SGSDK_Input;
 		SGSDK_Core.SetIcon(iconFilename);
 	end;
 	
+	procedure ChangeScreenSize(width, height: Integer); cdecl; export;
+	begin
+		SGSDK_Core.ChangeScreenSize(width, height);
+	end;
+	
+
+	procedure ToggleFullScreen(); cdecl; export;
+	begin
+		SGSDK_Core.ToggleFullScreen();
+	end;
+	
+	procedure RefreshScreen(); cdecl; export;
+	begin
+		SGSDK_Core.RefreshScreen();
+	end;
+	
+	procedure TakeScreenshot(basename: String); cdecl; export;
+	begin
+		SGSDK_Core.TakeScreenshot(basename);
+	end;
+	
+	function  ScreenWidth(): Integer; cdecl; export;
+	begin
+		result := SGSDK_Core.ScreenWidth();
+	end;
+	
+	function  ScreenHeight(): Integer; cdecl; export;
+	begin
+		result := SGSDK_Core.ScreenHeight();
+	end;
+	
+	function	ToSDLColor(color: UInt32): TSDL_Color; cdecl; export;
+	begin
+		result := SGSDK_Core.ToSDLColor(color);
+	end;
+	
+	//function	GetColour(forBitmap: Bitmap; apiColor: Color): Colour; overload; cdecl; export;
+	//begin
+		//result := SGSDK_Core.GetColour(forBitmap, apiColor);
+	//end;
+	
+	function	GetColour(red, green, blue, alpha: Byte) : Colour; cdecl; export;
+	begin
+		result := SGSDK_Core.GetColour(red, green, blue, alpha);
+	end;
+	
+	//function	GetColour(red, green, blue : Byte) : Colour; overload; cdecl; export;
+	//begin
+		//result := SGSDK_Core.GetColour(red, green, blue);	
+	//end;
+	
+	function	GetFramerate(): Integer; cdecl; export;
+	begin
+		result := SGSDK_Core.GetFramerate();
+	end;
+	
+	function	GetTicks(): UInt32; cdecl; export;
+	begin
+		result := SGSDK_Core.GetTicks();
+	end;
+	
+	procedure Sleep(time : UInt32); cdecl; export;
+	begin
+		SGSDK_Core.Sleep(time);
+	end;
+	
+	function GetPathToResource(filename: String; kind: ResourceKind) : String; overload; cdecl; export;
+	begin
+		result := SGSDK_Core.GetPathToResource(filename, kind);
+	end;
+	
+	//function GetPathToResource(filename: String): String; overload; cdecl; export;
+	//begin
+		//result := SGSDK_Core.GetPathToResource(filename);
+	//end;
+	
+	procedure RegisterEventProcessor(handle: EventProcessPtr; handle2: EventStartProcessPtr); cdecl; export;
+	begin
+		SGSDK_Core.RegisterEventProcessor(handle, handle2);
+	end;
+	
+	function Cos(angle: Single): Single; cdecl; export;
+	begin
+		result := SGSDK_Core.Cos(angle);
+	end;
+	
+	function Sin(angle: Single): Single; cdecl; export;
+	begin
+		result := SGSDK_Core.Sin(angle);
+	end;
+	
+	function Tan(angle: Single): Single; cdecl; export;
+	begin
+		result := SGSDK_Core.Tan(angle);
+	end;
+	
 	//INPUT
 exports
 	//CORE
 	OpenGraphicsWindow,
 	WindowCloseRequested,
-	ProcessEvents;
+	ProcessEvents,
+	SetIcon,
+	ChangeScreenSize,
+	ToggleFullScreen,
+	RefreshScreen,
+	TakeScreenShot,
+	ScreenWidth,
+	ScreenHeight,
+	ToSDLColor,
+	GetColour,	
+	GetFramerate,	
+	GetTicks,	
+	Sleep,	
+	GetPathToResource,
+	RegisterEventProcessor,	
+	Cos,
+	Sin,
+	Tan;
+	
+	
 	
 	//INPUT
-	;
+	
 end.
