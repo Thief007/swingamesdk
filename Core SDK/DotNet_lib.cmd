b897031096f6d44cf6265ced21faf8a6dd1507c8
grinneh <grinneh@c530d8a1-c93f-0410-abe6-152cf4f4075f>
2007-12-03 23:37:31 +0000

    DOTNET script now works

--- Core SDK/DotNet_lib.cmd
+++ Core SDK/DotNet_lib.cmd
@@ -1,33 +1,49 @@
 @echo off
 
+echo Please run with Visual Studio Command Prompt
+
 set BaseDir=%CD%
 echo Running script from %BaseDir%
 
-set Output=.\SGSDK.NET\lib\
+set Output1=.\SGSDK.NET\lib\
 echo Saving output to %Output%
+
+set Output2=.\SGSDK.NET\lib\
+echo Saving output to %Output2%
 
 set LibDir=%BaseDir%\lib\Win
 echo Getting libraries from %LibDir%
+
+set DOTNETsln=.\SGSDK.NET\src\
+
+set DOTNETbin=.\SGSDK.NET\src\bin\Debug\
 
 echo Doing %1
 
 if "%1"=="clean" goto cleaning
 
-	fpc -v0 -Mdelphi -FE"%Output%" .\src\SGSDK.pas
+	fpc -v0 -Mdelphi -FE"%Output1%" .\src\SGSDK.pas
 	
-	copy "%LibDir%\*.dll" "%Output%"
+	copy "%LibDir%\*.dll" "%Output1%"
 
-	del /Q "%Output%\*.o"
-	del /Q "%Output%\*.ppu"
-	del /Q "%Output%\*.a"
+	del /Q "%Output1%\*.o"
+	del /Q "%Output1%\*.ppu"
+	del /Q "%Output1%\*.a"
+
+	msbuild "SGSDK.NET\src\SGSDK.NET.sln"
+
+	copy "%DOTNETbin%\*.dll" "%Output2%"
+	copy "%DOTNETbin%\*.xml" "%Output2%"
+	
+
 
 	echo Finished
 goto end
 
 :cleaning
-	del /Q "%Output%\SGSDK*.*"
-	del /Q "%Output%\sdl*.*"
-	del /Q "%Output%\libimp*.*"
-	del /Q "%Output%\smpeg.*"
+	del /Q "%Output1%\SGSDK*.*"
+	del /Q "%Output1%\sdl*.*"
+	del /Q "%Output1%\libimp*.*"
+	del /Q "%Output1%\smpeg.*"
 	echo Cleaned
 :end 
