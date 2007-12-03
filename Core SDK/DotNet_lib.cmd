78867e53b9ab9e3993a0a45fe6a061492427438e
grinneh <grinneh@c530d8a1-c93f-0410-abe6-152cf4f4075f>
2007-12-03 04:23:23 +0000

    Simple script to compile the libraries for the SGSDK.NET

---
+++ Core SDK/DotNet_lib.cmd
@@ -0,0 +1,29 @@
+@echo off
+
+set BaseDir=%CD%
+echo Running script from %BaseDir%
+
+set Output=.\SGSDK.NET\lib\
+echo Saving output to %Output%
+
+set LibDir=%BaseDir%\lib\Win
+echo Getting libraries from %LibDir%
+
+echo Doing %1
+
+if "%1"=="clean" goto cleaning
+
+	fpc -v0 -Mdelphi -FE"%Output%" .\src\SGSDK.pas
+	
+	copy "%LibDir%\*.dll" "%Output%"
+
+	echo Finished
+goto end
+
+:cleaning
+	del /Q "%Output%\SGSDK*.*"
+	del /Q "%Output%\sdl*.*"
+	del /Q "%Output%\libimp*.*"
+	del /Q "%Output%\smpeg.*"
+	echo Cleaned
+:end 
