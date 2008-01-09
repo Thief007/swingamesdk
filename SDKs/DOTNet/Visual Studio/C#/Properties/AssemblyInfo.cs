b952742640cffc5cb9a885413c089aa2bab5d04f
grinneh <grinneh@c530d8a1-c93f-0410-abe6-152cf4f4075f>
2008-01-09 05:16:21 +0000

    Fixes

--- SDKs/DOTNet/Visual Studio/C#/Properties/AssemblyInfo.cs
+++ SDKs/DOTNet/Visual Studio/C#/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RPGDemo")]
+[assembly: AssemblyTitle("SwinGame")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RPGDemo")]
+[assembly: AssemblyProduct("SwinGame")]
 [assembly: AssemblyCopyright("Copyright ©  2007")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
