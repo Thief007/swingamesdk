f0cfaff3b4dad4896ca24df664f3ea44861e7e60
swinbrain <swinbrain@c530d8a1-c93f-0410-abe6-152cf4f4075f>
2008-01-14 11:21:46 +0000

    Fixed the unix scripts... woops

--- SDKs/DOTNet/unx_cp_common.sh
+++ SDKs/DOTNet/unx_cp_common.sh
@@ -1,23 +1,28 @@
 #!/bin/sh
 
-cp ../../Base/All/* ./Mono/C#
+cp ../Base/All/* ./Mono/C#
 
-if [ -d ./Mono/C#/Resources ]
+if [ ! -d ./Mono/C#/Resources ]
 then
+	echo Creating Resources Directory
 	mkdir ./Mono/C#/Resources
 	mkdir ./Mono/C#/Resources/fonts
 	mkdir ./Mono/C#/Resources/images
 	mkdir ./Mono/C#/Resources/sounds
 fi
 
-cp ../../Base/All/Resources/* ./Mono/C#/Resources 
-cp ../../Base/All/Resources/fonts/* ./Mono/C#/Resources/fonts
-cp ../../Base/All/Resources/images/* ./Mono/C#/Resources/images
-cp ../../Base/All/Resources/sounds/* ./Mono/C#/Resources/sounds
-cp ../../Base/DOTNet/*.cs ./Mono/C#
+echo Copying Resources
+cp ../Base/All/Resources/* ./Mono/C#/Resources 
+cp ../Base/All/Resources/fonts/* ./Mono/C#/Resources/fonts
+cp ../Base/All/Resources/images/* ./Mono/C#/Resources/images
+cp ../Base/All/Resources/sounds/* ./Mono/C#/Resources/sounds
 
-if [ -d ./Mono/C#/Properties ]
+echo Copying Code
+cp ../Base/DOTNet/*.cs ./Mono/C#
+cp ../Base/DOTNet/*.csproj ./Mono/C#
+
+if [ ! -d ./Mono/C#/Properties ]
 then
 	mkdir ./Mono/C#/Properties
 fi
-cp ../../Base/DOTNet/Properties/* ./Mono/C#/Properties
+cp ../Base/DOTNet/Properties/* ./Mono/C#/Properties
