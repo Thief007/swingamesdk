c5d99f06efe781071fe80a73c8a0d88720a46da2
aki.sexy <aki.sexy@c530d8a1-c93f-0410-abe6-152cf4f4075f>
2008-01-24 22:07:36 +0000

    Graphics tests fixed

--- Tests/Full/Pascal/GraphicsTests.pas
+++ Tests/Full/Pascal/GraphicsTests.pas
@@ -6,7 +6,7 @@ interface
 	procedure AddGraphicsSuite(var suites: TestSuites); 
 
 implementation
-	uses GameResources, SGSDK_Core, SGSDK_Input, SGSDK_Graphics, SGSDK_KeyCodes, SGSDK_Shapes;
+	uses GameResources, SGSDK_Core, SGSDK_Input, SGSDK_Graphics, SGSDK_KeyCodes, SGSDK_Shapes, SGSDK_Font;
 	
 	const
 		BOXDIM = 50;
@@ -24,7 +24,7 @@ implementation
 	
 	procedure TestDrawEllipse(const drawIn: Rectangle);
 	begin
-		ClearSurface(smallScreen);
+		ClearSurface(smallScreen, ColourTransparent);
 		if IsKeyPressed(VK_RIGHT) then tempRect.x := tempRect.x + 2;
 		if IsKeyPressed(VK_LEFT) then tempRect.x := tempRect.x - 2;
 		if IsKeyPressed(VK_UP) then tempRect.y := tempRect.y - 2;
@@ -48,29 +48,29 @@ implementation
 		tempRect4.width := tempRect.width;
 		tempRect4.height := tempRect.height;
 		DrawEllipse(smallScreen, ColourGreen, tempRect4);
-		DrawBitmap(smallScreen, 0, 70);
+		DrawBitmap(smallScreen, drawIn.x, drawIn.y);
 
-		DrawEllipse(ColourWhite, filled, 0, 70, tempRect.width, tempRect.height);
-		tempRect2.x := tempRect.width;
-		tempRect2.y := 70 + tempRect.height;
+		DrawEllipse(ColourWhite, filled, drawIn.x, drawIn.y, tempRect.width, tempRect.height);
+		tempRect2.x := tempRect.width + drawIn.x;
+		tempRect2.y := drawIn.y + tempRect.height;
 		tempRect2.width := tempRect.width;
 		tempRect2.height := tempRect.height;
 		DrawEllipse(ColourWhite, filled, tempRect2);
-		DrawEllipse(ColourWhite, 0, 70 + tempRect.height, tempRect.width, tempRect.height);
-		tempRect3.x := tempRect.width;
+		DrawEllipse(ColourWhite, drawIn.x, drawIn.y + tempRect.height, tempRect.width, tempRect.height);
+		tempRect3.x := tempRect.width + drawIn.x;
 		tempRect3.width := tempRect.width;
 		tempRect3.height := tempRect.height;
 		DrawEllipse(ColourWhite, tempRect3);
 
-		DrawEllipseOnScreen(ColourYellow, filled, 0, 580 - tempRect.height, tempRect.width, tempRect.height);
-		tempRect5.x := tempRect.width;
-		tempRect5.y := 580 - tempRect.height * 2;
+		DrawEllipseOnScreen(ColourYellow, filled, Round(drawIn.x), Round(RectangleBottom(drawIn) - tempRect.height), tempRect.width, tempRect.height);
+		tempRect5.x := tempRect.width + drawIn.x;
+		tempRect5.y := RectangleBottom(drawIn) - tempRect.height * 2;
 		tempRect5.width := tempRect.width;
 		tempRect5.height := tempRect.height;
 		DrawEllipseOnScreen(ColourYellow, filled, tempRect5);
-		DrawEllipseOnScreen(ColourYellow, 0, 580 - tempRect.height * 2, tempRect.width, tempRect.height);
-		tempRect6.x := tempRect.width;
-		tempRect6.y := 580 - tempRect.height;
+		DrawEllipseOnScreen(ColourYellow, Round(drawIn.x), Round(RectangleBottom(drawIn) - tempRect.height * 2), tempRect.width, tempRect.height);
+		tempRect6.x := tempRect.width + drawIn.x;
+		tempRect6.y := RectangleBottom(drawIn) - tempRect.height;
 		tempRect6.width := tempRect.width;
 		tempRect6.height := tempRect.height;
 		DrawEllipseOnScreen(ColourYellow, tempRect6);
@@ -78,7 +78,7 @@ implementation
 	
 	procedure TestDrawCircle(const drawIn: Rectangle);
 	begin
-		ClearSurface(smallScreen);
+		ClearSurface(smallScreen, ColourTransparent);
 		if IsKeyPressed(VK_RIGHT) then tempPoint.x := tempPoint.x + 2;
 		if IsKeyPressed(VK_LEFT) then tempPoint.x := tempPoint.x - 2;
 		if IsKeyPressed(VK_UP) then tempPoint.y := tempPoint.y - 2;
@@ -96,30 +96,31 @@ implementation
 		tempPoint2.x := tempPoint.x + curRadius * 2;
 		tempPoint2.y := tempPoint.y;
 		DrawCircle(smallScreen, ColourGreen, tempPoint2, curRadius);
-		DrawBitmap(smallScreen, 0, 70);
+		DrawBitmap(smallScreen, drawIn.x, drawIn.y);
 		
-		DrawCircle(ColourWhite, filled, curRadius, 70 + curRadius, curRadius);
-		tempPoint3.x := curRadius * 3;
-		tempPoint3.y := 70 + curRadius * 3;
+		DrawCircle(ColourWhite, filled, curRadius + drawIn.x, drawIn.y + curRadius, curRadius);
+		tempPoint3.x := curRadius * 3 + drawIn.x;
+		tempPoint3.y := drawIn.y + curRadius * 3;
 		DrawCircle(ColourWhite, filled, tempPoint3, curRadius);
-		DrawCircle(ColourWhite, curRadius, 70 + curRadius * 3, curRadius);
-		tempPoint4.x := curRadius * 3;
-		tempPoint4.y := 70 + curRadius;
+		DrawCircle(ColourWhite, curRadius + drawIn.x, drawIn.y + curRadius * 3, curRadius);
+		tempPoint4.x := curRadius * 3 + drawIn.x;
+		tempPoint4.y := drawIn.y + curRadius;
 		DrawCircle(ColourWhite, tempPoint4, curRadius);
 		
-		DrawCircleOnScreen(ColourYellow, filled, curRadius, 580 - curRadius, curRadius);
-		tempPoint5.x := curRadius * 3;
-		tempPoint5.y := 580 - curRadius * 3;
+		DrawCircleOnScreen(ColourYellow, filled, Round(curRadius + drawIn.x), Round(RectangleBottom(drawIn) - curRadius) - 1, curRadius);
+		tempPoint5.x := curRadius * 3 + drawIn.x;
+		tempPoint5.y := RectangleBottom(drawIn) - curRadius * 3 - 1;
 		DrawCircleOnScreen(ColourYellow, filled, tempPoint5, curRadius);
-		DrawCircleOnScreen(ColourYellow, curRadius, 580 - curRadius * 3, curRadius);
-		tempPoint6.x := curRadius * 3;
-		tempPoint6.y := 580 - curRadius;
+		DrawCircleOnScreen(ColourYellow, Round(curRadius + drawIn.x), Round(RectangleBottom(drawIn) - curRadius * 3) - 1, curRadius);
+		tempPoint6.x := curRadius * 3 + drawIn.x;
+		tempPoint6.y := RectangleBottom(drawIn) - curRadius - 1;
 		DrawCircleOnScreen(ColourYellow, tempPoint6, curRadius);
 	end;
 	
 	procedure TestDrawRectangle(const drawIn: Rectangle);
 	begin
-		ClearSurface(smallScreen);
+		ClearSurface(smallScreen, ColourTransparent);
+		
 		if IsKeyPressed(VK_RIGHT) then tempRect.x := tempRect.x + 2;
 		if IsKeyPressed(VK_LEFT) then tempRect.x := tempRect.x - 2;
 		if IsKeyPressed(VK_UP) then tempRect.y := tempRect.y - 2;
@@ -143,29 +144,29 @@ implementation
 		tempRect4.width := tempRect.width;
 		tempRect4.height := tempRect.height;
 		DrawRectangle(smallScreen, ColourGreen, tempRect4);
-		DrawBitmap(smallScreen, 0, 70);
+		DrawBitmap(smallScreen, drawIn.x, drawIn.y);
 		
-		DrawRectangle(ColourWhite, filled, 0, 70, tempRect.width, tempRect.height);
-		tempRect2.x := tempRect.width;
-		tempRect2.y := 70 + tempRect.height;
+		DrawRectangle(ColourWhite, filled, drawIn.x, drawIn.y, tempRect.width, tempRect.height);
+		tempRect2.x := tempRect.width + drawIn.x;
+		tempRect2.y := drawIn.y + tempRect.height;
 		tempRect2.width := tempRect.width;
 		tempRect2.height := tempRect.height;
 		DrawRectangle(ColourWhite, filled, tempRect2);
-		DrawRectangle(ColourWhite, 0, 70 + tempRect.height, tempRect.width, tempRect.height);
-		tempRect3.x := tempRect.width;
+		DrawRectangle(ColourWhite, drawIn.x, drawIn.y + tempRect.height, tempRect.width, tempRect.height);
+		tempRect3.x := tempRect.width + drawIn.x;
 		tempRect3.width := tempRect.width;
 		tempRect3.height := tempRect.height;
 		DrawRectangle(ColourWhite, tempRect3);
 		
-		DrawRectangleOnScreen(ColourYellow, filled, 0, 580 - tempRect.height, tempRect.width, tempRect.height);
-		tempRect5.x := tempRect.width;
-		tempRect5.y := 580 - tempRect.height * 2;
+		DrawRectangleOnScreen(ColourYellow, filled, Round(drawIn.x), Round(RectangleBottom(drawIn) - tempRect.height) - 1, tempRect.width, tempRect.height);
+		tempRect5.x := tempRect.width + drawIn.x;
+		tempRect5.y := RectangleBottom(drawIn) - tempRect.height * 2 - 1;
 		tempRect5.width := tempRect.width;
 		tempRect5.height := tempRect.height;
 		DrawRectangleOnScreen(ColourYellow, filled, tempRect5);
-		DrawRectangleOnScreen(ColourYellow, 0, 580 - tempRect.height * 2, tempRect.width, tempRect.height);
-		tempRect6.x := tempRect.width;
-		tempRect6.y := 580 - tempRect.height;
+		DrawRectangleOnScreen(ColourYellow, Round(drawIn.x), Round(RectangleBottom(drawIn) - tempRect.height * 2) - 1, tempRect.width, tempRect.height);
+		tempRect6.x := tempRect.width + drawIn.x;
+		tempRect6.y := RectangleBottom(drawIn) - tempRect.height - 1;
 		tempRect6.width := tempRect.width;
 		tempRect6.height := tempRect.height;
 		DrawRectangleOnScreen(ColourYellow, tempRect6);
@@ -190,11 +191,8 @@ implementation
 								+ EOL + 'Z : Shrink the box height' + EOL + 'X : Expand the box height' + EOL + 'T : Toggle fill' + EOL
 								+ 'White : Normal' + EOL + 'Green : On destination bitmap' + EOL + 'Yellow: On screen';
 			tempRect := CreateRectangle(0, 0, 50, 50);
-			tempRect2 := CreateRectangle(0, 0, 50, 50);
-			tempRect3 := CreateRectangle(0, 70, 50, 50);
+			tempRect3 := CreateRectangle(0, 129, 50, 50);
 			tempRect4 := CreateRectangle(0, 0, 50, 50);
-			tempRect5 := CreateRectangle(0, 0, 50, 50);
-			tempRect6 := CreateRectangle(0, 580, 50, 50);
 			smallScreen := CreateBitmap(500, 510);
 			filled := false;
 			ToRun := @TestDrawRectangle;
@@ -206,11 +204,6 @@ implementation
 			Instructions :=  	'Use the arrow keys to move' + EOL + 'the green circles.' + EOL + 'Z : Shrink the circle radius' + EOL + 'X : Expand the circle radius'
 								+ EOL + 'T : Toggle fill' + EOL + 'White : Normal' + EOL + 'Green : On destination bitmap' + EOL + 'Yellow: On screen';
 			tempPoint := CreatePoint(15, 15);
-			tempPoint2 := CreatePoint(0, 0);
-			tempPoint3 := CreatePoint(0, 0);
-			tempPoint4 := CreatePoint(0, 0);
-			tempPoint5 := CreatePoint(0, 0);
-			tempPoint6 := CreatePoint(0, 0);
 			curRadius := 15;
 			ToRun := @TestDrawCircle;
 		end;
