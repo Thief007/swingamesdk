0447cee863dc8ae0bbb483921e0cdb2ef0fb405b
swinbrain <swinbrain@c530d8a1-c93f-0410-abe6-152cf4f4075f>
2008-02-19 12:03:32 +0000

    Fixed scripts with spaces in the path name

--- Demos/VB.NET/copy_lib_mono.sh
+++ Demos/VB.NET/copy_lib_mono.sh
@@ -3,7 +3,7 @@
 # Move to src dir
 APP_PATH=`echo $0 | awk '{split($0,patharr,"/"); idx=1; while(patharr[idx+1] != "") { if (patharr[idx] != "/") {printf("%s/", patharr[idx]); idx++ }} }'`
 APP_PATH=`cd "$APP_PATH"; pwd` 
-cd $APP_PATH
+cd "$APP_PATH"
 
 Usage()
 {
@@ -30,10 +30,18 @@ then
 	rm -rf ./lib
 fi
 
-echo "   Making new lib folder"
+if [ -d ./My\ Project ] 
+then
+	echo "   Removing old My Project dir"
+	rm -rf ./My\ Project
+fi
+
+echo "   Making new folders"
 mkdir ./lib
+mkdir ./My\ Project
 
 LIB_PATH=../../../SDKs/DOTNet/Mono/VB
+LIB_PATH=`cd $LIB_PATH; pwd`
 
 echo "   Copying lib from ${LIB_PATH}"
 cp  -R ${LIB_PATH}/lib/* ./lib
--- SDKs/DOTNet/Mono/C#/pack.sh
+++ SDKs/DOTNet/Mono/C#/pack.sh
@@ -3,7 +3,7 @@
 # Move to src dir
 APP_PATH=`echo $0 | awk '{split($0,patharr,"/"); idx=1; while(patharr[idx+1] != "") { if (patharr[idx] != "/") {printf("%s/", patharr[idx]); idx++ }} }'`
 APP_PATH=`cd "$APP_PATH"; pwd` 
-cd $APP_PATH
+cd "$APP_PATH"
 
 echo "__________________________________________________"
 echo "Package Mac Application"
