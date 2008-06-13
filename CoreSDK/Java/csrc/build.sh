23f6c0b0b2a323cf051166b975220fc3e2e50522
swinbrain <swinbrain@c530d8a1-c93f-0410-abe6-152cf4f4075f>
2008-06-13 06:02:55 +0000

    Added javah to build.sh

--- CoreSDK/Java/csrc/build.sh
+++ CoreSDK/Java/csrc/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+javah -classpath ../build/classes swingame.Core swingame.Audio swingame.Input swingame.Graphics
+
 gcc -dynamiclib ../../SGSDK.NET/lib/*.o -I$JAVA_HOME/Include `cat ../../src/maclinki386.res` *.c -o ../build/classes/libJavaSwinGame.jnilib -framework JavaVM -framework Cocoa -framework Foundation
