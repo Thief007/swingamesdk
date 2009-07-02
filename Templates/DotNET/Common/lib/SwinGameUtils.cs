c674605ad9f6242dac9ea4da89e8b71b40be901d
swinbrain <swinbrain@c530d8a1-c93f-0410-abe6-152cf4f4075f>
2009-07-02 00:28:54 +0000

    Added SwinGameUtils for C#

---
+++ Templates/DotNET/Common/lib/SwinGameUtils.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SwinGame
+{
+    internal static class Utils
+    {
+        static LineSegment[] FromPtr()
+        {
+            return null;
+        }
+    }
+}
