ebc852375bc80545c582971493bc292216beb314
v4mp1r3.david <v4mp1r3.david@c530d8a1-c93f-0410-abe6-152cf4f4075f>
2010-01-05 22:43:33 +0000

    Labels and Buttons now working, more functionality, can check what was clicked, can show/hide activate/deactivate panels.

--- CoreSDK/src/sgUserInterface.pas
+++ CoreSDK/src/sgUserInterface.pas
@@ -23,104 +23,206 @@ interface
 
 type
 	GUILabelData = record
-           contentString: string;
-           textFont: Font;
-    end;
-	
+    contentString: string;
+    fontID:       String;
+  end;
+  
 	GUILabel = ^GUILabelData;
 	
 	RegionData = record
-           kind: integer;
-           elementIndex: integer;
-           area: Rectangle;
-           active: boolean;
-           parent: ^Panel;
-    end;
+    stringID:     String;
+    kind:         LongInt;
+    regionID:     LongInt;
+    elementIndex: LongInt;
+    area:         Rectangle;
+    active:       boolean;
+    parent:       ^Panel;
+  end;
 
 	Region = ^RegionData;
 	
-    PanelData = record
-          stringID: string;
-          position: Point2D;
-		  width: integer;
-		  height: integer;
-          visible: boolean;
-          active: boolean;
-          panelBitmap: Bitmap;
-          regions: Array of Region;
-          RegionName: Array of String;
-          //Hash table of Region strings -> ints
-          labels: Array of GUILabel;
-    end;
+  PanelData = record
+    stringID:     String;
+    panelID:      LongInt;
+    position:     Point2D;
+    width:        LongInt;
+    height:       LongInt;
+    visible:      boolean;
+    active:       boolean;
+    panelBitmap:  Bitmap;
+    regions:      Array of Region;
+    regionIds:    NamedIndexCollection;
+    labels: Array of GUILabel;
+  end;
 	
 	Panel = ^PanelData;
 	
 	GUIController = record
-          panels: Array of Panel;
-          visiblePanels: Array of Panel;
-          globalGUIFont: Font;
-    end;
+    panels:         Array of Panel;
+    panelIds:       NamedIndexCollection;
+    visiblePanels:  Array of Panel;
+    globalGUIFont:  Font;
+    globalGUIVectorColor: Color;
+    VectorDrawing:  Boolean;
+  end;
 
 function LoadPanel(filename: string): Panel;
 procedure ShowPanel(p: Panel);
 procedure AddPanelToGUI(p: Panel);
-procedure DrawPanelsAsRectangles();
-
+procedure DrawPanels();
+procedure SetGUIColorForVectors(c:Color);
+procedure DrawGUIAsVectors(b : boolean);
+procedure HidePanel(p: Panel);
+function PanelClicked(): Panel;
+function RegionClicked(): Region;
+function RegionClicked(pnl: Panel): Region; Overload;
+function RegionStringID(r: Region): string;
+procedure ToggleShowPanel(p: Panel);
+procedure ActivatePanel(p: Panel);
+procedure DeactivatePanel(p: Panel);
+procedure ToggleActivatePanel(p: Panel);
 
 //=============================================================================
 implementation
   uses
     SysUtils, StrUtils, Classes, 
     stringhash, MyStrUtils, sgNamedIndexCollection,   // libsrc
-    sgShared, sgResources, sgTrace, sgImages, sgGraphics, sgCore;
+    sgShared, sgResources, sgTrace, sgImages, sgGraphics, sgCore, sgGeometry, sgText, sgInput;
 //=============================================================================
 
 var
   _Panels: TStringHash;
   GUIC: GUIController;
   
-procedure DrawPanels(gC: GUIController);
+procedure DrawAsVectors();
 var
-   i: integer;
+  i, j: integer;
 begin
-    for i := Low(gC.panels) to High(gC.panels) do
-	begin
-		if (gC.panels[i]^.visible) then
-		begin
-			DrawBitmapOnScreen(gC.panels[i]^.panelBitmap, gC.panels[i]^.position);
-		end;
+  for i := Low(GUIC.panels) to High(GUIC.panels) do
+  begin
+    if (GUIC.panels[i]^.visible) then
+    begin
+      DrawRectangleOnScreen(GUIC.globalGUIVectorColor, Trunc(GUIC.panels[i]^.position.x), Trunc(GUIC.panels[i]^.position.y), GUIC.panels[i]^.width, GUIC.panels[i]^.height);
+      for j := Low(GUIC.panels[i]^.Regions) to High(GUIC.panels[i]^.Regions) do
+      begin
+        if GUIC.panels[i]^.Regions[j]^.kind = 0 then
+          DrawRectangleOnScreen(GUIC.globalGUIVectorColor, GUIC.panels[i]^.Regions[j]^.area)
+        else if GUIC.panels[i]^.Regions[j]^.kind = 1 then
+        begin
+          DrawTextOnScreen(GUIC.panels[i]^.Labels[GUIC.panels[i]^.Regions[j]^.elementIndex]^.contentString, GUIC.GlobalGUIVectorColor, FetchFont(GUIC.panels[i]^.Labels[GUIC.panels[i]^.Regions[j]^.elementIndex]^.fontID), Trunc(GUIC.panels[i]^.Regions[j]^.area.x), Trunc(GUIC.panels[i]^.Regions[j]^.area.y));
+        end;
+      end;
     end;
+  end;
 end;
 
-procedure DrawPanelsAsRectangles();
+procedure DrawAsBitmaps();
 var
-   i: integer;
+  i: integer;
 begin
-    for i := Low(GUIC.panels) to High(GUIC.panels) do
-	begin
-		if (GUIC.panels[i]^.visible) then
-		begin
-			DrawRectangleOnScreen(RGBColor(0,255,0), Trunc(GUIC.panels[i]^.position.x), Trunc(GUIC.panels[i]^.position.y), GUIC.panels[i]^.width, GUIC.panels[i]^.height);
-		end;
-    end;
+  for i := Low(GUIC.panels) to High(GUIC.panels) do
+  begin
+    if (GUIC.panels[i]^.visible and not(GUIC.panels[i]^.panelBitmap = nil)) then
+      DrawBitmapOnScreen(GUIC.panels[i]^.panelBitmap, GUIC.panels[i]^.position) //Look up syntax...
+    else
+      RaiseException('Bitmap for panel ' + IntToStr(i) + ' not set. Please check your input file or enable vector drawing.');
+  end;
+end;
+  
+procedure DrawPanels();
+begin
+  if GUIC.VectorDrawing then
+    DrawAsVectors()
+  else
+    DrawAsBitmaps();
 end;
 
+function RegionStringID(r: Region): string;
+begin
+  if assigned(r) then
+    result := r^.stringID
+  else
+    result := '';
+end;
+
+procedure ActivatePanel(p: Panel);
+begin
+  if assigned(p) then p^.active := true;
+end;
+
+procedure DeactivatePanel(p: Panel);
+begin
+  if assigned(p) then p^.active := false;
+end;
+
+procedure ToggleActivatePanel(p: Panel);
+begin
+  if assigned(p) then p^.active := not p^.active;
+end;
 
 procedure ShowPanel(p: Panel);
 begin
-	p^.visible := true;
+  if assigned(p) then p^.visible := true;
+end;
+
+procedure ToggleShowPanel(p: Panel);
+begin
+  if assigned(p) then p^.visible := not p^.visible;
 end;
 
 procedure HidePanel(p: Panel);
 begin
-	p^.visible := false;
+	if assigned(p) then p^.visible := false;
 end;
 
-function regionClicked(): Region;
+function PanelClicked(): Panel;
 var
-	res: Region;
+  i: integer;
 begin
-	result := res;
+  result := nil;
+  if not MouseClicked(Leftbutton) then exit;
+  for i:= Low(GUIC.panels) to High(GUIC.panels) do
+  begin
+    if (GUIC.panels[i]^.active) then
+    begin
+      if PointInRect(MousePosition(), GUIC.panels[i]^.position.x,GUIC.panels[i]^.position.y,GUIC.panels[i]^.width,GUIC.panels[i]^.height) then
+      result := GUIC.panels[i];
+      exit;
+    end;
+  end;
+end;
+
+function RegionClicked(pnl: Panel): Region; Overload;
+var
+  i, j: integer;
+begin
+result := nil;
+  if not MouseClicked(Leftbutton) then exit;
+  if pnl = nil then exit;
+  if (pnl^.active) then
+  begin
+    for j := Low(pnl^.Regions) to High(pnl^.Regions) do
+    begin
+      if PointInRect(MousePosition(), pnl^.Regions[j]^.area) then
+      begin
+        result := pnl^.Regions[j];
+        exit;
+      end;
+    end;
+  end;
+end;
+
+function RegionClicked(): Region;
+var
+  i: integer;
+begin
+  result := nil;
+  if not MouseClicked(Leftbutton) then exit;
+  for i := Low(GUIC.panels) to High(GUIC.panels) do
+  begin
+    Result := RegionClicked(GUIC.panels[i]);
+    if not(result = nil) then exit;
+  end;
 end;
 
 procedure setLabelText(var l: GUILabel; s: string);
@@ -128,28 +230,15 @@ begin
 	l^.contentString := s;
 end;
 
-procedure setLabelFont(var l: GUILabel; f: Font);
+procedure setLabelFont(var l: GUILabel; s: String);
 begin
-	l^.textFont := f;
-end;
-
-procedure AddRegionToPanel(r: Region; p: Panel);
-var
-	i: integer;
-begin
-	SetLength(p^.Regions, Length(p^.Regions) + 1);
-	p^.Regions[Length(p^.Regions) - 1] := r;
-	r^.parent := @p;
+	l^.fontID := s;
 end;
 
 procedure AddPanelToGUI(p: Panel);
 begin
-	WriteLn('In add panel to GUI');
-	//WriteLn(HexStr(g));
 	SetLength(GUIC.panels, Length(GUIC.panels) + 1);
-	WriteLn('Length set :', Length(GUIC.panels));
-	GUIC.panels[Length(GUIC.panels) - 1] := p;
-	WriteLn('Panel added');
+	GUIC.panels[High(GUIC.panels)] := p;
 end;
 
 function CreateButton(area: Rectangle; active: boolean): Region;
@@ -158,16 +247,8 @@ begin
 	result^.kind := 0; // Sets as button
 	result^.area := area; // 
 	result^.active := active; //Sets whether the element can be clicked or not.
-    result^.parent := nil; //Set when region is added to panel.
+  result^.parent := nil; //Set when region is added to panel.
 	// Panel is set when region is added to panel.
-end;
-
-function CreateGUILabel(area: Rectangle; active: boolean): Region;
-begin
-	New(result);
-	result^.kind := 1; // Sets as GUILabel
-	result^.area := area;
-	
 end;
 
 function LoadPanel(filename: string): Panel;
@@ -192,7 +273,58 @@ var
 			result := StrToInt(str);
 		end;
 	end;
-	
+  
+  procedure CreateLabel(forRegion: Region; d: string);
+  var
+    newLbl: GUILabel;
+  begin
+    New(newLbl);
+    newLbl^.contentString := ExtractDelimited(7, d, [',']);
+    newLbl^.fontID := Trim(ExtractDelimited(8, d, [',']));
+    
+    SetLength(result^.Labels, Length(result^.Labels) + 1);
+    result^.labels[High(result^.labels)] := newLbl;
+    forRegion^.elementIndex := High(result^.labels);  // The label index for the region -> so it knows which label
+  end;
+  
+  procedure AddRegionToPanelWithString(d: string; p: panel);
+  var
+    regID: string;
+    regX, regY, regW, regH, regKind, addedIdx: integer;
+    r: region;
+  begin
+    regX := StrToInt(Trim(ExtractDelimited(1, d, [','])));
+    regY := StrToInt(Trim(ExtractDelimited(2, d, [','])));
+    regW := StrToInt(Trim(ExtractDelimited(3, d, [','])));
+    regH := StrToInt(Trim(ExtractDelimited(4, d, [','])));
+    regKind := StrToInt(Trim(ExtractDelimited(5, d, [','])));
+    
+    regID := Trim(ExtractDelimited(6, d, [',']));
+    
+    regX += Trunc(p^.position.x);
+    regY += Trunc(p^.position.y);
+    
+    new(r);
+    r^.area := RectangleFrom(regX, regY, regW, regH);
+    r^.kind := regKind;
+    r^.active := true;
+    r^.StringID := regID;
+    
+    addedIdx := AddName(result^.regionIds, regID);   //Allocate the index
+    SetLength(p^.Regions, Length(p^.Regions) + 1);
+    r^.RegionID := High(p^.Regions);
+    if High(p^.Regions) <> addedIdx then begin RaiseException('Error creating panel - added index is invalid.'); exit; end;
+    
+    p^.Regions[addedIdx] := r;
+    r^.parent := @p;
+    
+    case r^.kind of
+      0: ;
+      1: CreateLabel(r,d);
+    end;
+    
+  end;
+  
 	procedure ProcessLine();
 	begin
 		// Split line into id and data around :
@@ -211,6 +343,7 @@ var
 			'y': result^.position.y := MyStrToInt(data, false);
 			'w': result^.width := MyStrToInt(data, false);
 			'h': result^.height := MyStrToInt(data, false);
+      'r': AddRegionToPanelWithString(data, result);
 		else
 			begin
 				RaiseException('Error at line ' + IntToStr(lineNo) + ' in panel: ' + filename + '. Error with id: ' + id + '. This should be one of the characters defined in the template.');
@@ -218,6 +351,24 @@ var
 			end;
 		end;
 	end;
+  
+  procedure InitPanel();
+  var
+    P2D: Point2D;
+  begin
+    P2D.x := 0;
+    P2D.y := 0;
+  	New(result);
+    result^.stringID := '';
+    result^.panelBitmap := nil;
+    result^.panelID := -1;
+    result^.position := P2D;
+    result^.width := -1;
+    result^.height := -1;
+    result^.visible := false;
+    result^.active := false;
+    InitNamedIndexCollection(result^.regionIds);   //Setup the name <-> id mappings
+  end;
 	
 begin
 	{$ifdef Windows}
@@ -226,7 +377,8 @@ begin
 	pathToFile := PathToResourceWithBase(applicationPath, 'panels/' + filename);
 	{$endif}
 	
-	New(result);
+  // Initialise the resulting panel
+  InitPanel();
 	
 	Assign(panelFile, pathToFile);
 	Reset(panelFile);
@@ -234,16 +386,26 @@ begin
 	lineNo := -1;
 	
 	while not EOF(panelFile) do
-    begin
-		lineNo := lineNo + 1;
-		
-		ReadLn(panelFile, line);
-		line := Trim(line);
-		if Length(line) = 0 then continue;  //skip empty lines
-		if MidStr(line,1,2) = '//' then continue; //skip lines starting with // - the first character at index 0 is the length of the string.
-		
-		ProcessLine();
-    end;
+  begin
+    lineNo := lineNo + 1;
+    
+    ReadLn(panelFile, line);
+    line := Trim(line);
+    if Length(line) = 0 then continue;  //skip empty lines
+    if MidStr(line,1,2) = '//' then continue; //skip lines starting with // - the first character at index 0 is the length of the string.
+  
+    ProcessLine();
+  end;
+end;
+
+procedure SetGUIColorForVectors(c:Color);
+begin
+  GUIC.globalGUIVectorColor := c;
+end;
+
+procedure DrawGUIAsVectors(b : boolean);
+begin
+  GUIC.VectorDrawing := b;
 end;
 
 //=============================================================================
@@ -254,6 +416,7 @@ end;
     {$ENDIF}
     
     InitialiseSwinGame();
+    GUIC.VectorDrawing := False;
     
     _Panels := TStringHash.Create(False, 1024);
     
--- CoreSDK/test/GUITests.pas
+++ CoreSDK/test/GUITests.pas
@@ -1,33 +1,55 @@
-program HelloWorld;
+program GUITests;
 //{IFNDEF UNIX} {r GameLauncher.res} {ENDIF}
 
 uses
-  sgCore, sgUserInterface, sgAudio, sgGraphics, sgResources, sgText;
+  sgCore, sgUserInterface, sgAudio, sgGraphics, sgResources, sgText, sgGeometry, sgTypes;
 	
+  
+procedure InitInterface(var pnla, pnlb: panel);
+begin
+  SetGUIColorForVectors(ColorWhite);
+  
+  pnla := LoadPanel('panelwithbutton.txt');
+  pnlb := LoadPanel('panelwithlabel.txt');
+  ShowPanel(pnla);
+  HidePanel(pnlb);
+  
+  ActivatePanel(pnla);
+  ActivatePanel(pnlb);
+  
+  AddPanelToGUI(pnla);
+  AddPanelToGUI(pnlb);
+  
+  DrawGUIAsVectors(true);
+end;
+  
+procedure UpdateInterface(pnla, pnlb: panel);
+begin
+  if RegionStringID(RegionClicked(PanelClicked())) = 'Button1' then
+  begin
+    WriteLn('Button1 Clicked');
+    ToggleShowPanel(pnlb);
+  end;
+end;
+  
 procedure Main();
 var
-	pnl: Panel;
-	i: integer;
+	pnla, pnlb: Panel;
 begin
   OpenAudio();
   
   OpenGraphicsWindow('Hello World', 800, 600);
   
-  //LoadResourceBundle('MainMenu.txt');
-  //New(GUIC);
+  LoadResourceBundle('MainMenu.txt');
 
-  pnl := LoadPanel('main_menu.txt');
-  WriteLn('Panel loaded.');
-  ShowPanel(pnl);
-  WriteLn('Panel shown successfully.');
-  AddPanelToGUI(pnl);
-  WriteLn('Panel added to GUIC successfully.');
+  InitInterface(pnla,pnlb);
   
   repeat
     ProcessEvents();
     ClearScreen(ColorBlack);
     
-	DrawPanelsAsRectangles();
+    DrawPanels();
+    UpdateInterface(pnla, pnlb);
 	
     DrawFramerate(0,0);
     RefreshScreen();
