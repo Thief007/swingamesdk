fce680106a26ce5da3ec67e5d848c534f320ce83
soulknightv2 <soulknightv2@c530d8a1-c93f-0410-abe6-152cf4f4075f>
2010-01-20 04:55:17 +0000

    Character with new direction state handling, hide/show layer

--- CoreSDK/src/sgCharacters.pas
+++ CoreSDK/src/sgCharacters.pas
@@ -20,43 +20,129 @@ interface
 //=============================================================================
 
 type
-	CharacterData = record
-		CharSprite: Sprite;
-		States: NamedIndexCollection;
-		Directions: NamedIndexCollection;
-		CurrentState: LongInt;
-		CurrentDirection: LongInt;
-		DrawOrder: Array of Array of LongInt;
-		DrawLayer: Array of Array of Boolean;
-		CharValueNames: NamedIndexCollection;
-		CharValues: Array of LongInt;
-		DirectionParameters: Array of LongInt;
-		CharName : String;
-		CharType: String;
+	DirectionAngles = record
+		min : LongInt;
+		max : LongInt;
+	end;
+	
+	DirStateData = record
+		Anim : LongInt;
+		LayerOrder: Array of LongInt;
 	end;
 	
 	Character = ^CharacterData;
+	LayerCache = Array of Array of Array of LongInt;
+
+	CharacterData = record
+		CharSprite		 				: Sprite;
+		CharName  						: String;
+		CharType  						: String;
+		States	 							: NamedIndexCollection;
+		Directions						: NamedIndexCollection;
+		CharValueNames 				: NamedIndexCollection;
+		LastAngle 						: LongInt;
+		CurrentState	 				: LongInt;
+		CurrentDirection	 		: LongInt;
+		DirAngles							: Array of DirectionAngles;
+		CharValues						: Array of LongInt;
+		DirectionParameters 	: Array of LongInt;
+		ShownLayers						: Array of Boolean;
+		ShownLayersByDirState	: Array of Array of DirStateData;
+		ShownLayerCache				: LayerCache;
+	end;
+		
 	
 	function LoadCharacter(filename: String) : Character;
-	procedure DrawCharacter(c: Character);
+	procedure DrawCharacterWithStationary(var c: Character; state: Integer);
+	function UpdateDirectionAnimation(var c: Character) : Boolean;
+	function UpdateDirectionAnimationWithStationary(var c: Character; state, newState: integer) : Boolean;
+	procedure HideShowLayer(c: Character; index: integer);
 
 //=============================================================================
 implementation
-	  uses
-    SysUtils, StrUtils, Classes, 
-    stringhash, MyStrUtils, sgNamedIndexCollection,   // libsrc
-    sgShared, sgResources, sgTrace, sgAudio, sgImages, sgSprites, sgAnimations;
+	uses
+		crt, sgCore, sgAudio, sgAnimations, sgGeometry, sgResources,
+		sgImages, sgInput, sgPhysics, sgNamedIndexCollection, sgShared,
+		sgSprites, SysUtils, MyStrUtils, stringhash, StrUtils;
 //=============================================================================	
-{
-	function SetupDirectionParameters() : Array of LongInt;
-	begin
-	end; }
 	
+	procedure SetActiveLayer(var c: Character);
+	begin
+		c^.CharSprite^.visibleLayers := c^.ShownLayerCache[c^.CurrentState, c^.CurrentDirection];
+	end;
 
-	
-	procedure HideShowLayer(c: Character; layerID: string; show: boolean);
+	function UpdateDirectionAnimationWithStationary(var c: Character; state, newState: integer) : Boolean;
 	begin
-		c^.DrawLayer[(c^.CurrentDirection * NameCount(c^.States) + c^.CurrentState) ,IndexOf(c^.CharSprite^.layerIDs, layerID)] := show;
+		result := false;
+		if (c^.CharSprite^.velocity.x = 0) AND (c^.CharSprite^.velocity.y = 0) then 
+		begin
+			if c^.CurrentState = state then exit;
+			c^.CurrentState := state;
+			SpriteStartAnimation(c^.CharSprite, c^.ShownLayersByDirState[c^.CurrentState, c^.CurrentDirection].Anim);
+			result:= true;
+		end
+		else begin
+			if c^.CurrentState <> newState then
+			begin
+				c^.CurrentState := newState;
+				SetActiveLayer(c);
+				SpriteStartAnimation(c^.CharSprite, c^.ShownLayersByDirState[c^.CurrentState, c^.CurrentDirection].Anim);
+			end;
+			result := UpdateDirectionAnimation(c);
+		end;
+	end;
+	
+	function UpdateShownLayerCache(c: Character): LayerCache;
+	var
+		states, directions, layers, count : Integer;
+	begin
+		SetLength(result, NameCount(c^.States), NameCount(c^.Directions));
+		
+		for states := Low(result) to High(result) do
+		begin
+			for directions := Low(result[states]) to High(result[states]) do
+			begin
+				count := 0;
+				for layers := Low(c^.ShownLayersByDirState[states, directions].LayerOrder) to High(c^.ShownLayersByDirState[states, directions].LayerOrder) do
+				begin
+					if c^.ShownLayers[c^.ShownLayersByDirState[states, directions].LayerOrder[layers]] then
+					begin
+						SetLength(result[states,directions], Length(result[states,directions]) + 1);
+						result[states,directions,count] := c^.ShownLayersByDirState[states, directions].LayerOrder[layers];
+						count += 1;
+					end;
+				end;
+			end;
+		end;		
+	end;
+
+	function UpdateDirectionAnimation(var c: Character) : Boolean;
+	var
+		i : Integer;
+		angle : single;
+	begin
+		angle := VectorAngle(c^.CharSprite^.velocity);
+		result := false;
+		for i := 0 to NameCount(c^.Directions) -1 do
+		begin
+			if (i = c^.CurrentDirection) then continue;
+			
+			if ((c^.DirAngles[i].min < c^.DirAngles[i].max) AND (angle >= c^.DirAngles[i].min) AND (angle <= c^.DirAngles[i].max)) OR 
+			(((c^.DirAngles[i].min > 0) AND (c^.DirAngles[i].max < 0)) AND (((angle >= c^.DirAngles[i].min) AND (angle <= 180)) OR ((angle <= c^.DirAngles[i].max) AND (angle >= -180)))) then
+			begin
+				c^.CurrentDirection := i;
+				SetActiveLayer(c);
+				SpriteStartAnimation(c^.CharSprite, c^.ShownLayersByDirState[c^.CurrentState, c^.CurrentDirection].Anim);
+				result := true;
+			end
+		end;
+	end; 
+	
+	procedure HideShowLayer(c: Character; index: integer);
+	begin
+		c^.ShownLayers[index] := not c^.ShownLayers[index];
+		c^.ShownLayerCache := UpdateShownLayerCache(c);
+		SetActiveLayer(c);
 	end;
 	
 	procedure SetCharacterName(var c: Character; name : String);
@@ -78,30 +164,16 @@ implementation
 	begin
 		c^.CurrentDirection := direction;
 	end;
-	
-	procedure SetActiveLayer(c : Character);
-	var
-		drawIndex, i, count: integer;
-		newDrawOrder : Array of LongInt;
-	begin
-		drawIndex := c^.CurrentDirection * NameCount(c^.States) + c^.CurrentState;
-		count := 0;
-		
-		for i := Low(c^.DrawOrder[drawIndex]) to High(c^.DrawOrder[drawIndex]) do
-		begin
-			if c^.DrawLayer[drawIndex,i] then
-			begin
-				SetLength(newDrawOrder, Length(newDrawOrder) + 1);
-				newDrawOrder[count] := c^.DrawOrder[drawIndex,i];
-				count += 1;
-			end;
-		end;		
-		c^.CharSprite^.visibleLayers := newDrawOrder;
-	end;
 
 	procedure FreeCharacter(c : character);
 	begin
 		Dispose(c);
+	end;
+	
+	procedure DrawCharacterWithStationary(var c: character; state: integer);
+	begin
+		UpdateDirectionAnimationWithStationary(c,0, state);
+		DrawSprite(c^.CharSprite);
 	end;
 	
 	procedure DrawCharacter(c: character);
@@ -113,7 +185,8 @@ implementation
 	var
 		bmpArray: Array of Bitmap;
 		data, line, id, path, name: string;
-		lineno, maxid: integer;
+		lineno, maxid, w, h, cols, rows, count, i: integer;
+		ShownLayers : Array of Boolean;
 		
 		function MyStrToInt(str: String; allowEmpty: Boolean) : LongInt;
 		begin
@@ -133,6 +206,15 @@ implementation
 			end;
 		end;
 		
+		procedure SetCellDetails();
+		begin
+			w 	 	:= StrToInt(ExtractDelimited(1, data, [',']));
+			h 	 	:= StrToInt(ExtractDelimited(2, data, [',']));
+			cols 	:= StrToInt(ExtractDelimited(3, data, [',']));
+			rows 	:= StrToInt(ExtractDelimited(4, data, [',']));
+			count := StrToInt(ExtractDelimited(5, data, [',']));
+		end;
+		
 		procedure SetName();
 		begin
 			if Length(ExtractDelimited(2, line, [':'])) > 0 then SetCharacterName(result, ExtractDelimited(1, data, [',']));
@@ -147,12 +229,13 @@ implementation
 		var
 			i: integer;
 		begin
-			SetLength(bmpArray, StrToInt(ExtractDelimited(1, data, [','])));
-			for i := 0 to StrToInt(ExtractDelimited(1, data, [','])) - 1 do
+			SetLength(bmpArray, CountDelimiter(data, ',') + 1);
+			
+			for i := Low(bmpArray) to High(bmpArray) do
 			begin
-				WriteLn(ExtractDelimited(i + 2, data, [',']));
-				bmpArray[i] := LoadBitmap(ExtractDelimited(i + 2, data, [',']));
-				WriteLn(ExtractDelimited(i + 2, data, [',']));
+				bmpArray[i] := LoadBitmap(ExtractDelimited(i+1, data, [',']));
+				SetTransparentColor(bmpArray[i], RGBColor(255,255,255));
+				SetBitmapCellDetails(bmpArray[i], w, h, cols, rows, count);
 			end;
 			result^.CharSprite := CreateSprite(bmpArray);
 		end;
@@ -174,8 +257,6 @@ implementation
 			for i := 0 to StrToInt(ExtractDelimited(2, data, [','])) -1 do
 			begin
 				AddName(result^.Directions, ExtractDelimited(i + 3, data, [',']));
-				SetLength(result^.DrawOrder, Length(result^.DrawOrder) + 1);
-				SetLength(result^.DrawLayer, Length(result^.DrawLayer) + 1);
 			end;
 		end;
 		
@@ -189,48 +270,60 @@ implementation
 			for i := 0 to StrToInt(ExtractDelimited(2, data, [','])) -1 do
 			begin
 				AddName(result^.States, ExtractDelimited(i + 3, data, [',']));
-				SetLength(result^.DrawOrder, Length(result^.DrawOrder) + 1);
-				SetLength(result^.DrawLayer, Length(result^.DrawLayer) + 1);
 			end;
 		end;
 		
-		procedure SetDrawOrder();
+		procedure SetAngles();
 		var
-			drawRange: string;
-			id : integer;
+			i: integer;
 		begin
-			drawRange := ExtractDelimited(2, data, [',']);
-			id := IndexOf(result^.Directions, ExtractDelimited(2, data, [',']));
-			
-			if id <> -1 then result^.DrawOrder[id] := ProcessRange(ExtractDelimitedWithRanges(1, drawRange));
-
+			SetLength(result^.DirAngles, Length(result^.DirAngles) + 1);
+			i := IndexOf(result^.Directions, ExtractDelimited(1, data, [',']));
+			result^.DirAngles[i].min := StrToInt(ExtractDelimited(2, data, [',']));
+			result^.DirAngles[i].max := StrToInt(ExtractDelimited(3, data, [',']));
 		end;
 		
-		procedure SetDrawLayerBooleans();
+		procedure SetDirectionStateDetails();
+		var
+			dirIndex, stateIndex, aniIndex: integer;
+		begin
+			stateIndex := IndexOf(result^.States, ExtractDelimited(1, data, [',']));
+			dirIndex	 := IndexOf(result^.Directions, ExtractDelimited(2, data, [',']));
+			
+			if High(result^.ShownLayersByDirState) < stateIndex then SetLength(result^.ShownLayersByDirState, stateIndex + 1);
+			if High(result^.ShownLayersByDirState[stateIndex]) < dirIndex then SetLength(result^.ShownLayersByDirState[stateIndex], dirIndex + 1);
+						
+			result^.ShownLayersByDirState[stateIndex,dirIndex].Anim := IndexOf(result^.CharSprite^.animationTemplate^.animationIDs, ExtractDelimited(3, data, [',']));
+			result^.ShownLayersByDirState[stateIndex,dirIndex].LayerOrder := ProcessRange(ExtractDelimitedWithRanges(4, data));
+		end;
+		
+		procedure SetShownLayersBooleans();
 		var
 			i, id: integer;
 			draw: string;
 		begin			
-			id := IndexOf(result^.Directions, ExtractDelimited(2, data, [',']));
-			if id <> -1 then
-			begin				
-				for i := 0 to Length(bmpArray) -1 do
-				begin
-					draw := ExtractDelimited(3 + i, data, [',']);
-					SetLength(id, Length(result^.DrawLayer[id]) + 1);
-					if draw = 't' then result^.DrawLayer[id,i] := true
-					else if draw = 'f' then result^.DrawLayer[id,i] := false
-					else begin
-						RaiseException('Error at line ' + IntToStr(lineNo) + ' in character ' + filename + '. Error with DrawLayer: ' + ExtractDelimited(3 + i, data, [',']) + '. This should be one of f (false) or t (true)');
-						exit;
-					end; 
-				end;
+			SetLength(result^.ShownLayers, Length(bmpArray));
+			for i := Low(bmpArray) to High(bmpArray) do
+			begin
+				draw := ExtractDelimited(i + 1, data, [',']);
+				if draw = 't' then result^.ShownLayers[i] := true
+				else if draw = 'f' then result^.ShownLayers[i] := false
+				else begin
+					RaiseException('Error at line ' + IntToStr(lineNo) + ' in character ' + filename + '. Error with ShownLayers: ' + ExtractDelimited(3 + i, data, [',']) + '. This should be one of f (false) or t (true)');
+					exit;
+				end; 
 			end;
 		end;
 		
 		procedure AddAniTemplateToChar();
 		begin
 			result^.CharSprite^.animationTemplate := LoadAnimationTemplate(ExtractDelimited(1, data, [',']));
+		end;
+		
+		procedure InitializeAnimation();
+		begin
+			result^.CurrentDirection := IndexOf(result^.Directions, data);
+			SpriteStartAnimation(result^.CharSprite, result^.ShownLayersByDirState[result^.CurrentState, result^.CurrentDirection].Anim);
 		end;
 			
 		procedure ProcessLine();
@@ -239,28 +332,31 @@ implementation
 			id := ExtractDelimited(1, line, [':']);
 			data := ExtractDelimited(2, line, [':']);
 			
-			// Verify that id is a single char
-			if Length(id) <> 1 then
-			begin
-				RaiseException('Error at line ' + IntToStr(lineNo) + ' in animation ' + filename + '. Error with id: ' + id + '. This should be a single character.');
-				exit;
-			end;
 			WriteLn(data);
 			// Process based on id
-			case LowerCase(id)[1] of // in all cases the data variable is read
-				'n': SetName();
-				't': SetType();
-				'b': AddBitmapToCharacterSprite();
-				'a': AddAniTemplateToChar();
-				'd': SetDirections();
-				's': SetStates();
-				'v': AddValuesToCharacter();				
-{				else
-				begin
-					RaiseException('Error at line ' + IntToStr(lineNo) + ' in animation ' + filename + '. Error with id: ' + id + '. This should be one of f,m,i or s.');
-					exit;
-				end; }
-			end;
+			
+			if Length(id) = 2 then SetDirectionStateDetails()
+			
+			else
+			
+				case LowerCase(id)[1] of // in all cases the data variable is read
+					'n': SetName();
+					't': SetType();
+					'b': AddBitmapToCharacterSprite();
+					'a': AddAniTemplateToChar();
+					'd': SetDirections();
+					's': SetStates();
+					'v': AddValuesToCharacter();	
+					'l': SetShownLayersBooleans();		
+					'c': SetCellDetails();		
+					'p': SetAngles();		
+					'i': InitializeAnimation();									
+	{				else
+					begin
+						RaiseException('Error at line ' + IntToStr(lineNo) + ' in animation ' + filename + '. Error with id: ' + id + '. This should be one of f,m,i or s.');
+						exit;
+					end; }
+				end;
 		end;
 			
 		procedure VerifyVersion();
@@ -286,8 +382,6 @@ implementation
 		
 		path := PathToResource('characters\' + filename);
 		lineNo := 0;
-		maxId := -1;
-//		result := nil;
 		
 		Assign(input, path);
 		Reset(input);
@@ -297,7 +391,7 @@ implementation
 		New(result);
 		
 		InitNamedIndexCollection(result^.CharValueNames);
-		
+				
 		try
 			while not EOF(input) do
 			begin	
@@ -314,6 +408,10 @@ implementation
 		finally
 			Close(input);
 		end;
+		
+		result^.ShownLayerCache := UpdateShownLayerCache(result);
+		SetActiveLayer(result);
+		
 		{$IFDEF TRACE}
 			TraceExit('sgCharacters', 'LoadCharacter');
 		{$ENDIF}  
