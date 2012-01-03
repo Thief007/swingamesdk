4d15e54598897932f3ec77fa9af304187628049c
abidhasan89 <abidhasan89@gmail.com>
2012-01-03 22:34:37 +0000

    Added primitive shapes how to code for pascal

---
+++ CoreSDK/test/HowToDrawPrimitiveShapes.pas
@@ -0,0 +1,29 @@
+program HowToDrawPrimitiveShapes;
+uses sgAudio, sgGraphics, sgUtils, sgResources;
+
+procedure Main();
+begin
+    OpenAudio();
+    OpenGraphicsWindow('Primitive shapes', 800, 600);
+    
+	ClearScreen();
+	
+	DrawRectangle(ColorGreen, 25, 100, 200, 100 );
+	FillRectangle(ColorGreen, 25, 400, 200, 100 );
+	
+	DrawTriangle(ColorYellow, 275, 200, 475, 200, 375, 100);
+	FillTriangle(ColorYellow, 275, 500, 475, 500, 375, 400);
+	
+	DrawCircle(ColorWhite, 625, 150, 100);
+	
+	RefreshScreen();
+    
+    Delay(5000);
+
+    ReleaseAllResources();
+    CloseAudio();
+end;
+	
+begin
+	Main();
+end.
