686f493110e6591dac35d5dfb1d060ad274abd64
abidhasan89 <abidhasan89@gmail.com>
2012-01-05 22:33:25 +0000

    "How To" code for creating an animation

---
+++ CoreSDK/test/HowToCreateAnAnimation.pas
@@ -0,0 +1,37 @@
+program HowToCreateAnAnimation;
+uses sgGraphics, sgSprites, sgTypes, sgImages, sgUtils, sgInput, sgAudio, sgAnimations;
+
+procedure Main();
+var
+	explosion: Sprite;
+	explosionBmp: Bitmap;
+begin
+    OpenAudio();
+	OpenGraphicsWindow('Exploding a Sprite', 800, 600);
+
+	explosionBmp := LoadBitmapNamed('explosion', 'explosion_pro.png');
+    BitmapSetCellDetails(explosionBmp, 72, 72, 6, 7, 45);
+    LoadAnimationScriptNamed('explosionScrpt', 'explosion.txt');
+	
+	explosion := CreateSprite(explosionBmp, AnimationScriptNamed('explosionScrpt'));
+	SpriteStartAnimation(explosion, 'explosion');
+	
+	SpriteSetX(explosion, 400);
+    SpriteSetY(explosion, 300);
+	
+	repeat
+		ClearScreen(ColorWhite);
+		DrawSprite(explosion);
+		RefreshScreen();
+		
+		UpdateSprite(explosion);
+		
+		ProcessEvents();
+	until SpriteAnimationHasEnded(explosion);
+	
+	Delay(800);
+end;
+
+begin
+	Main();
+end.
