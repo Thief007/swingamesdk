8691443f0e59801d6875942428db3363fbfefe38
abidhasan89 <abidhasan89@gmail.com>
2012-01-06 00:37:25 +0000

    "How To" code for creating multiple animation from one animation script in pascal

---
+++ CoreSDK/test/HowToCreateMultipleAnimations.pas
@@ -0,0 +1,68 @@
+program HowToCreateAnAnimation;
+uses sgGraphics, sgSprites, sgTypes, sgImages, sgUtils, sgInput, sgAudio, sgAnimations;
+
+procedure Main();
+var
+	explosion: Sprite;
+	implosion: Sprite;
+	explosion_loop: Sprite;
+	explosionBmp: Bitmap;
+begin
+    OpenAudio();
+	OpenGraphicsWindow('Miltiple Animations', 800, 600);
+
+	explosionBmp := LoadBitmapNamed('explosion', 'explosion_pro.png');
+    BitmapSetCellDetails(explosionBmp, 72, 72, 6, 7, 45);
+    LoadAnimationScriptNamed('explosionScrpt', 'explosion.txt');
+	
+	explosion := CreateSprite(explosionBmp, AnimationScriptNamed('explosionScrpt'));
+	implosion := CreateSprite(explosionBmp, AnimationScriptNamed('explosionScrpt'));
+	explosion_loop := CreateSprite(explosionBmp, AnimationScriptNamed('explosionScrpt'));
+	
+	SpriteStartAnimation(explosion, 'explosion');
+	SpriteStartAnimation(implosion, 'implosion');
+	SpriteStartAnimation(explosion_loop, 'explosion_loop');
+	
+	SpriteSetX(explosion, 200);
+    SpriteSetY(explosion, 100);
+	
+    SpriteSetX(implosion, 400);
+	SpriteSetY(implosion, 300);
+	
+	SpriteSetX(explosion_loop, 600);
+	SpriteSetY(explosion_loop, 500);
+	
+	ClearScreen(ColorWhite);
+	
+	repeat
+		ClearScreen(ColorWhite);
+		DrawSprite(explosion);
+		RefreshScreen();
+		UpdateSprite(explosion);
+		ProcessEvents();
+	until SpriteAnimationHasEnded(explosion);
+	
+	delay(1500);
+	
+	repeat
+		ClearScreen(ColorWhite);
+		DrawSprite(implosion);
+		RefreshScreen();
+		UpdateSprite(implosion);
+		ProcessEvents();
+	until SpriteAnimationHasEnded(implosion);
+	
+	delay(1500);
+	
+	repeat
+		ClearScreen(ColorWhite);
+		DrawSprite(explosion_loop);
+		RefreshScreen();
+		UpdateSprite(explosion_loop);
+		ProcessEvents();
+	until WindowCloseRequested();
+end;
+
+begin
+	Main();
+end.
