9c0d4122f58265a05472c606c3f87d2d923cf110
abidhasan89 <abidhasan89@gmail.com>
2012-01-09 00:09:26 +0000

    finished code for drawing a text

--- CoreSDK/test/HowToDrawText.pas
+++ CoreSDK/test/HowToDrawText.pas
@@ -1,11 +1,23 @@
 program HowToDrawText;
-uses sgText;
+uses 
+	sgText, sgGraphics, sgUtils, sgResources, sgTypes;
 
 procedure Main();
-begin
-	DrawText();
-end;
+begin    
+    OpenGraphicsWindow('Drawing Text', 800, 600);
+    
+	ClearScreen();
+	
+	DrawText('You Win!!!', ColorGreen, 300, 200);
+	DrawTextLines('OR DO YOU???', ColorRed, ColorWhite, 'Arial', 18, AlignCenter, 400, 400, 160, 100);
+	
+	RefreshScreen();
+    
+    Delay(5000);
 
+    ReleaseAllResources();    
+end;
+	
 begin
 	Main();
 end.
