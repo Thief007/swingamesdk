41e8e8df521fceb25c1abf8eaf037e26c2792283
zz.ngu90 <zz.ngu90@gmail.com>
2012-01-12 05:03:34 +0000

    Rename HowToRepeatSound.pas to keep it inline with website and upload sound effect chipmunk.ogg to resource folder

--- CoreSDK/test/HowToRepeatSound.pas
+++ CoreSDK/test/HowToRepeatSound.pas
@@ -1,19 +1,19 @@
-program HowToRepeatingASound;
+program HowToRepeatSound;
 uses
   sgInput, sgGraphics, sgResources, sgText, sgAudio, sgUtils, sgTypes;
 
 procedure Main();
 begin  
   OpenAudio();
-  OpenGraphicsWindow('Repeating Sound Effect', 320, 240);
+  OpenGraphicsWindow('How To Repeat Sound', 320, 240);
   
   repeat // The game loop...
     ProcessEvents();    
 	
 	ClearScreen(ColorWhite);
 	
-	PlaySoundEffect(LoadSoundEffect('Siren.ogg')); // load sound effect in ogg, wave audio files	
-	DrawText('How To Play Sound Effect!!!', ColorRed, 10, 120);	
+	PlaySoundEffect(LoadSoundEffect('chipmunk.ogg')); // load sound effect in ogg, wave audio files	
+	DrawText('Repeat Sound Effect!!!', ColorRed, 55, 120);	
 	
     RefreshScreen();
   until WindowCloseRequested();
