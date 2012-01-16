bb7ad4cde96501ab2bf869fd7f61f16782c49bea
zz.ngu90 <zz.ngu90@gmail.com>
2012-01-16 23:05:41 +0000

    remove tab with space

--- CoreSDK/test/HowToControlMovement.pas
+++ CoreSDK/test/HowToControlMovement.pas
@@ -4,37 +4,38 @@ uses
   
 procedure HandleInput(var x,y: Single);
 begin
-	if KeyDown(vk_LEFT) then	x -= 1;
-	if KeyDown(vk_RIGHT) then x += 1;
-	if KeyDown(vk_UP) then y -= 1;
-	if KeyDown(vk_DOWN) then y += 1;
+  if KeyDown(vk_LEFT) then  x -= 1;
+  if KeyDown(vk_RIGHT) then x += 1;
+  if KeyDown(vk_UP) then y -= 1;
+  if KeyDown(vk_DOWN) then y += 1;
 end;
+  
   
 procedure Main();
 var 
-	x, y : Single;
-	w, h : LongInt;
-	c : Color;
+  x, y : Single;
+  w, h : LongInt;
+  c : Color;
 begin 
-	OpenGraphicsWindow('Moving Shape', 800, 600);
+  OpenGraphicsWindow('Moving Shape', 800, 600);
 
-	x := 300;
-	y := 250;
-	w := 200;
-	h := 100;
-	c := ColorGreen;
-	
-	repeat // The game loop...
-		ProcessEvents();
-    
-		ClearScreen(ColorWhite);	
-		FillRectangle(c, x, y, w, h);
-		HandleInput(x, y);
-	
-		RefreshScreen();
-	until WindowCloseRequested();
+  x := 300;
+  y := 250;
+  w := 200;
+  h := 100;
+  c := ColorGreen;
   
-	ReleaseAllResources();
+  repeat // The game loop...
+    ProcessEvents();
+    
+    ClearScreen(ColorWhite);  
+    FillRectangle(c, x, y, w, h);
+    HandleInput(x, y);
+  
+    RefreshScreen();
+  until WindowCloseRequested();
+  
+  ReleaseAllResources();
 end;
 
 begin
