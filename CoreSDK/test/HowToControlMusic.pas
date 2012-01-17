6f1c593f727da109d355f6426cc4516bd7f9dc77
zz.ngu90 <zz.ngu90@gmail.com>
2012-01-17 04:44:09 +0000

    ModelShapeUsingRecord, replace tab with space in HowToControlMusic.pas and delete some file

--- CoreSDK/test/HowToControlMusic.pas
+++ CoreSDK/test/HowToControlMusic.pas
@@ -3,7 +3,7 @@ uses
   sgInput, sgGraphics, sgResources, sgText, sgAudio, sgUtils, sgTypes;
 procedure Main();
 var
-  mus : Music;	
+  mus : Music;  
 begin
   OpenAudio();
 
@@ -18,7 +18,7 @@ begin
   SetMusicVolume(1);
 
   repeat // The game loop...
-    ProcessEvents();			
+    ProcessEvents();      
 
     ClearScreen(ColorWhite);
 
@@ -29,7 +29,7 @@ begin
       if KeyTyped(vk_3) then FadeMusicIn('gentle-thoughts-1.mp3', -1, 10000);
       if KeyTyped(vk_4) then FadeMusicIn('morning-workout.mp3', -1, 10000);
       if KeyTyped(vk_5) then FadeMusicIn('saber.ogg', -1, 10000);
-      if KeyTyped(vk_6) then FadeMusicIn('diving-turtle.mp3', -1, 10000);			
+      if KeyTyped(vk_6) then FadeMusicIn('diving-turtle.mp3', -1, 10000);      
     end
     else if KeyDown(vk_RALT) OR KeyDown(vk_LALT) then
     begin
@@ -46,11 +46,11 @@ begin
       if KeyTyped(vk_2) then PlayMusic('fast.mp3');
       if KeyTyped(vk_3) then PlayMusic('gentle-thoughts-1.mp3');
       if KeyTyped(vk_4) then PlayMusic('morning-workout.mp3');
-      if KeyTyped(vk_5) then PlayMusic('saber.ogg');			
+      if KeyTyped(vk_5) then PlayMusic('saber.ogg');      
       if KeyTyped(vk_6) then PlayMusic('diving-turtle.mp3');
       if KeyTyped(vk_p) then PauseMusic();
       if KeyTyped(vk_r) then ResumeMusic();
-			if KeyTyped(vk_KP_PLUS) and (MusicVolume() < 1)  then SetMusicVolume(MusicVolume + 0.1);
+      if KeyTyped(vk_KP_PLUS) and (MusicVolume() < 1)  then SetMusicVolume(MusicVolume + 0.1);
       if KeyTyped(vk_KP_MINUS) and (MusicVolume() > 0) then SetMusicVolume(MusicVolume - 0.1);      
       if KeyTyped(vk_s) then if MusicPlaying() then StopMusic();
     end;
@@ -63,7 +63,7 @@ begin
     DrawText('+ or - increase volume by 10%', ColorBlue, 'Arial', 14, 20, 175);
     DrawText('s to stop playing music', ColorBlue, 'Arial', 14, 20, 200);
 
-    RefreshScreen(60);		
+    RefreshScreen(60);    
   until WindowCloseRequested() OR KeyTyped(vk_ESCAPE) OR KeyTyped(VK_Q);
 
   CloseAudio();
