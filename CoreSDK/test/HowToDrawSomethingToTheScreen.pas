bbfb18d3b25d74cfab1e7f2320cf87ffe07ba919
zz.ngu90 <zz.ngu90@gmail.com>
2012-01-20 05:09:16 +0000

    Remove 4 line of code that not relevance to the How To

--- CoreSDK/test/HowToDrawSomethingToTheScreen.pas
+++ CoreSDK/test/HowToDrawSomethingToTheScreen.pas
@@ -5,17 +5,9 @@ procedure Main();
 begin
   OpenGraphicsWindow('How To Draw Something To The Screen');
   
-  ClearScreen();    // clear the background canvas
-  RefreshScreen();  // make it appear on screen...
-  Delay(2000);      // wait for 2 seconds
-  
-  ClearScreen(ColorYellow);   // clear the background canvas
-  RefreshScreen();            // make it appear on screen...
   ClearScreen(ColorWhite);    // clear the background canvas
-  Delay(2000);                // wait for 2 seconds... notice yellow screen!
-  
-  RefreshScreen();            // make the white screen appear...
-  Delay(2000);
+  RefreshScreen();  // make it appear on screen...
+  Delay(2000);      // wait for 2 seconds  
   
   ReleaseAllResources();
 end;
