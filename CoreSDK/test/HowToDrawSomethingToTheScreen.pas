12fddc7b3254390faa90418da9445e39de5bc3a1
swinbrain <swinbrain@c530d8a1-c93f-0410-abe6-152cf4f4075f>
2012-01-22 22:05:24 +0000

    Re-added code to the "How to draw something to the screen" code

--- CoreSDK/test/HowToDrawSomethingToTheScreen.pas
+++ CoreSDK/test/HowToDrawSomethingToTheScreen.pas
@@ -5,9 +5,20 @@ procedure Main();
 begin
   OpenGraphicsWindow('How To Draw Something To The Screen');
   
-  ClearScreen(ColorWhite);    // clear the background canvas
+  ClearScreen();    // clear the background canvas (black by default)
   RefreshScreen();  // make it appear on screen...
   Delay(2000);      // wait for 2 seconds  
+  
+  ClearScreen(ColorYellow);   // clear the background canvas
+  RefreshScreen();            // make it appear on screen...
+  
+  //Start drawing the "next" screen to appear...
+  ClearScreen(ColorWhite);    // clear the background canvas
+  
+  Delay(2000);                // wait for 2 seconds... notice the yellow screen!
+  RefreshScreen();            // make the white screen appear...
+  
+  Delay(2000);
   
   ReleaseAllResources();
 end;
