61d54294f82f903c178a15e3eca0839fb9a9f591
zz.ngu90 <zz.ngu90@gmail.com>
2012-01-25 04:30:19 +0000

    Replace tab with space

--- CoreSDK/test/HowToReadingTextToScreen.pas
+++ CoreSDK/test/HowToReadingTextToScreen.pas
@@ -4,21 +4,21 @@ sgTypes, sgInput, sgGraphics, sgResources, sgUtils, sgText;
 
 procedure Main();
 var
-	fnt : Font;
-begin	
-  OpenGraphicsWindow('How To Read Text To Screen', 240, 160);	
+  fnt : Font;
+begin  
+  OpenGraphicsWindow('How To Read Text To Screen', 240, 160);  
 
-	fnt := LoadFont('Arial',12);
+  fnt := LoadFont('Arial',12);
   repeat // The game loop...
     ProcessEvents();
-	
-		ClearScreen(ColorWhite);
-		
-		if NOT ReadingText() THEN
-		begin
-			StartReadingText(ColorRed, 40, fnt, 10, 10);						
-		end;
-		
+  
+    ClearScreen(ColorWhite);
+    
+    if NOT ReadingText() THEN
+    begin
+      StartReadingText(ColorRed, 40, fnt, 10, 10);            
+    end;
+    
     RefreshScreen(60);
   until WindowCloseRequested();
 
