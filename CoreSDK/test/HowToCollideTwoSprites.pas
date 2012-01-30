23c941c5f986eaadbf32b96edd62d40c82aa4537
abidhasan89 <abidhasan89@gmail.com>
2012-01-30 05:00:54 +0000

    "How To" code for two sprites bouncing off each other

---
+++ CoreSDK/test/HowToCollideTwoSprites.pas
@@ -0,0 +1,44 @@
+program HowToCreateASprite;
+uses
+    sgTypes, sgAudio, sgText, sgGraphics, sgGeometry, sgResources, sgSprites, sgInput, sgPhysics, sgImages;
+
+procedure Main();
+var
+    earth: Sprite;
+    asteroid: Sprite;
+begin
+    OpenGraphicsWindow('Create a Sprite', 800, 600);
+
+    ClearScreen(ColorWhite);
+
+    LoadBitmapNamed('earth', 'earth.png');
+    LoadBitmapNamed('asteroid', 'asteroid.png');
+
+    earth := CreateSprite(BitmapNamed('earth'));
+    SpriteSetX(earth, 700);
+    SpriteSetY(earth, 100);
+    SpriteSetVelocity(earth, VectorTo(-0.8, 0.6));
+
+    asteroid := CreateSprite(BitmapNamed('asteroid'));
+    SpriteSetX(asteroid, 100);
+    SpriteSetY(asteroid, 500);
+    SpriteSetVelocity(asteroid, VectorTo(1, -0.6));
+
+    repeat
+        ProcessEvents();
+        ClearScreen(ColorWhite);
+        DrawSprite(earth);
+        UpdateSprite(earth);
+        DrawSprite(asteroid);
+        UpdateSprite(asteroid);
+        if SpriteCollision(earth, asteroid) then CollideCircles(asteroid, earth);
+
+        RefreshScreen();
+    until WindowCloseRequested();
+
+    ReleaseAllResources();
+end;
+
+begin
+    Main();
+end.
