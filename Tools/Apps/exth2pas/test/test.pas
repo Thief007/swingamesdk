d762e4f6f176cca13f7c08afeb40239dc4cb6529
Soulknightv2 <soulknightv2@gmail.com>
2012-02-02 02:14:16 +0000

    Updated test.pas for SDL1.3 with strange behaviour

--- Tools/Apps/exth2pas/test/test.pas
+++ Tools/Apps/exth2pas/test/test.pas
@@ -1,15 +1,39 @@
-// fpc test.pas -k-F../Frameworks -k-framework -kSDL -k-macosx_version_min -k10.7 -k-no_pie
+// fpc test.pas -k"-rpath @loader_path/../Frameworks" -k"-F../Frameworks -framework SDL" -k"-macosx_version_min 10.7 -no_pie"
+
 program Test;
-uses SDL;
+uses SDL, Math;
 
 var
-    wnd: PSDL_Window;
-begin
-    wnd := SDL_CreateWindow(PChar('title:Pchar'), 0, 0, 800, 600, 0);
-    wnd := SDL_CreateWindow(PChar('title:Pchar'), 0, 0, 800, 600, 0);
+  win : PSDL_Window;
+  renderer : PSDL_Renderer;
+  srcRect : SDL_Rect;
+begin      
+  srcRect.x := 0;
+  srcRect.y := 0;
+  srcRect.w := 100;
+  srcRect.h :=100;
+  
+  //SDL_Init(SDL_INIT_VIDEO);
+  SetExceptionMask([exInvalidOp, exDenormalized, exZeroDivide, exOverflow, exUnderflow, exPrecision]);
+  
+  win := SDL_CreateWindow('Test', 100, 100, 800, 600, Uint32(SDL_WINDOW_SHOWN));
+  
+  renderer := nil;
+  renderer := SDL_CreateRenderer(win, -1, LongWord(SDL_RENDERER_ACCELERATED) or LongWord(SDL_RENDERER_PRESENTVSYNC));
+  WriteLn(HexStr(renderer));
+  
+ SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+ SDL_RenderClear(renderer);
+ SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); 
+  repeat
+{      srcRect.x +=1;
+      srcRect.y +=1;
+}      SDL_RenderFillRect(renderer, @srcRect);
+      SDL_RenderPresent(renderer);
+  until False;
+  
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(win);
     
-    WriteLn(HexStr(wnd));
-    
-    Readln();
-    
+	//ReleaseAllResources();
 end.
