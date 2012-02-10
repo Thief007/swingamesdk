ed4a273f317450e71d5c316f5d59bc3211b3a909
kaka.itachi <kaka.itachi@gmail.com>
2012-02-10 04:09:04 +0000

    removed draw code

--- CoreSDK/libsrc/drivers/OpenGL/sgDriverGraphicsOpenGL.pas
+++ CoreSDK/libsrc/drivers/OpenGL/sgDriverGraphicsOpenGL.pas
@@ -133,8 +133,8 @@ implementation
   
   procedure DrawCircleProcedure(dest: Bitmap; clr: Color; xc, yc: Single; radius: Longint);
   begin
-    glColor3f(0.0f, 0.0f, 0.0f);
-    glRectf(-0.75f,0.75f, 0.75f, -0.75f);
+    // glColor3f(0.0f, 0.0f, 0.0f);
+    // glRectf(-0.75f,0.75f, 0.75f, -0.75f);
   end;
 
   procedure FillCircleProcedure(dest: Bitmap; clr: Color; xc, yc: Single; radius: Longint);
