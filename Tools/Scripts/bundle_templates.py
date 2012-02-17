0d3dd5b6ce309389ee9a46ba2af3a5a71b950fb8
swinbrain <swinbrain@c530d8a1-c93f-0410-abe6-152cf4f4075f>
2012-02-17 05:48:53 +0000

    Added new scripts, required by packaging

--- Tools/Scripts/bundle_templates.py
+++ Tools/Scripts/bundle_templates.py
@@ -12,54 +12,6 @@ from swin_template_utils import *
 def clear_generated():
     pass
 
-# ==========================
-# = Create SGSDK libraries =
-# ==========================
-
-# for compiling SGSDK
-def copy_coresdk_to_dist_source():
-    generated_source_folder =   generated_folder + "Source/src/"
-    template_source_folder =    tempate_folder + "Source/"
-    lib_src_folder =            swingame_path + "CoreSDK/libsrc/"
-    src_folder =                swingame_path + "CoreSDK/src/"
-    lib_folder =                swingame_path + "CoreSDK/lib/"
-    
-    dist_source_folder =        dist_folder + "Source/"
-    dist_source_src_folder =    dist_source_folder + "src/"
-    dist_source_lib_folder =    dist_source_folder + "lib/"
-    
-    copy_without_svn(template_source_folder, dist_source_folder)
-    copy_without_svn(lib_folder, dist_source_lib_folder, overwrite = False)
-    flat_copy_without_svn(generated_source_folder, dist_source_src_folder)
-    flat_copy_without_svn(lib_src_folder, dist_source_src_folder)
-    flat_copy_without_svn(src_folder, dist_source_src_folder)
-
-
-_sgsdk_creation_script_options = {
-    'Mac OS X': [['-badass','-static'], ['-godly','-static'], None],            # default must be last
-    'Windows':  [None], #, '-badass', '-godly'],
-}
-
-def create_sgsdk_library():
-    """ The first step in creating the SwinGame templates is to create the
-        SGSDK framework/dll. This needs to be created for each of the various
-        backend versions supported by the platform."""
-    
-    sgsdk_build_script = swingame_path + 'Dist/Source/build.sh'
-    
-    output_header(['Generating the code for SGSDK.pas'])
-    run_python('create_sgsdk_code.py')
-    
-    output_line('Copying code to dist source directory')
-    copy_coresdk_to_dist_source()
-    
-    output_header(['Compiling SGSDK framework/dll'])
-    option_list = _sgsdk_creation_script_options[get_os_name()]
-    
-    for opt in option_list:
-        run_bash(sgsdk_build_script, opt)
-
-
 # =========================================
 # = Create the various language libraries =
 # =========================================
@@ -74,9 +26,8 @@ def create_lang_libraries():
           lang_template_dict['pre_copy_script']()
       
       for copy_dist in lang_template_dict['copy_dist']:
-          if get_os_name() in copy_dist['os']:
-              output_line("Packaging " + copy_dist["target"])
-              assemble_dist(key, copy_dist, lang_template_dict['use_sgsdk'])
+          output_line("Packaging " + copy_dist["target"])
+          assemble_dist(key, copy_dist, lang_template_dict['use_sgsdk'])
       
 
 #assembles the files for the dist folder
@@ -165,8 +116,6 @@ def assemble_dist(language, dist_dict, use_sgsdk):
 def main():
     output_header(['Packaging SwinGame Templates'])
     
-    # Create the framework/dll
-    create_sgsdk_library()
     create_lang_libraries()
     
     print("\nFinished!")
---
+++ Tools/Scripts/create_library.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+
+import sys
+import os
+import platform
+import subprocess
+import swin_shutil
+from swin_template_utils import *
+
+# ==========================
+# = Create SGSDK libraries =
+# ==========================
+
+# for compiling SGSDK
+def copy_coresdk_to_dist_source():
+    generated_source_folder =   generated_folder + "Source/src/"
+    template_source_folder =    tempate_folder + "Source/"
+    lib_src_folder =            swingame_path + "CoreSDK/libsrc/"
+    src_folder =                swingame_path + "CoreSDK/src/"
+    lib_folder =                swingame_path + "CoreSDK/lib/"
+    
+    dist_source_folder =        dist_folder + "Source/"
+    dist_source_src_folder =    dist_source_folder + "src/"
+    dist_source_lib_folder =    dist_source_folder + "lib/"
+    
+    copy_without_svn(template_source_folder, dist_source_folder)
+    copy_without_svn(lib_folder, dist_source_lib_folder, overwrite = False)
+    flat_copy_without_svn(generated_source_folder, dist_source_src_folder)
+    flat_copy_without_svn(lib_src_folder, dist_source_src_folder)
+    flat_copy_without_svn(src_folder, dist_source_src_folder)
+
+
+_sgsdk_creation_script_options = {
+    'Mac OS X': [['-badass','-static'], ['-godly','-static'], None],            # default must be last
+    'Windows':  [None], #, '-badass', '-godly'],
+}
+
+def create_sgsdk_library():
+    """ The first step in creating the SwinGame templates is to create the
+        SGSDK framework/dll. This needs to be created for each of the various
+        backend versions supported by the platform."""
+    
+    sgsdk_build_script = swingame_path + 'Dist/Source/build.sh'
+    
+    output_header(['Generating the code for SGSDK.pas'])
+    run_python('create_sgsdk_code.py')
+    
+    output_line('Copying code to dist source directory')
+    copy_coresdk_to_dist_source()
+    
+    output_header(['Compiling SGSDK framework/dll'])
+    option_list = _sgsdk_creation_script_options[get_os_name()]
+    
+    for opt in option_list:
+        run_bash(sgsdk_build_script, opt)
+
+
+if __name__ == '__main__':
+    output_header(['Packaging SwinGame Templates'])
+    
+    # Create the framework/dll
+    create_sgsdk_library()
+    
