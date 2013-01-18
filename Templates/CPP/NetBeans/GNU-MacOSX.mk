1082654dfd002244769ad5f6838674cfc0880193
swinbrain <swinbrain@c530d8a1-c93f-0410-abe6-152cf4f4075f>
2013-01-18 07:21:56 +0000

    Created NetBeans windows and linux versions! - nice - need to add to website

--- Templates/CPP/NetBeans/GNU-MacOSX.mk
+++ Templates/CPP/NetBeans/GNU-MacOSX.mk
@@ -1,4 +1,5 @@
 SWINGAME_FLAGS=-Wl,-rpath,@loader_path/../Frameworks -arch i386 -F./lib/sdl13/mac -framework SGSDK
+SWINGAME_LINK_FLAGS=
 
 .build-platform:
 	${MKDIR} -p ${CND_DISTDIR}/${CONF}/Frameworks
